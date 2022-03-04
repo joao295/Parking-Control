a4866ebb7c0c8cfbe5d06002f980edc121976725
João Almeida <78218571+joao295@users.noreply.github.com>
2022-03-04 06:57:58 -0300

    services

---
+++ java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,65 @@
+package com.api.parkingcontrol.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+
+	
+	
+	
+	final ParkingSpotRepository parkingSpotRepository;
+
+	public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+		this.parkingSpotRepository = parkingSpotRepository;
+	}
+	
+	
+	@Transactional
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
+
+
+	public boolean existsByLicensePlateCar(String licensePlateCar) {
+		return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+	}
+
+
+	public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+		return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+	}
+
+
+    public boolean existsByApartamentAndBlock(String apartament, String block) {
+        return parkingSpotRepository.existsByApartamentAndBlock(apartament, block);
+    }
+
+	 public List<ParkingSpotModel> finAll(){
+		return parkingSpotRepository.findAll();
+	}
+
+
+	public Optional<ParkingSpotModel> findById(UUID id) {
+		
+		return parkingSpotRepository.findById(id);
+	}
+
+
+
+	@Transactional
+	public void delete(ParkingSpotModel parkingSpotModel) {
+		parkingSpotRepository.delete(parkingSpotModel);
+		
+	}
+
+}
